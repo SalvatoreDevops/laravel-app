f380c91749c80ac187f8a99f6db54eb8a94e6d90
salvatore <salvarecr@gmail.com>
2024-04-26 18:13:55 +0200

    create form

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Show the create page for user.
+     */
+    public function create()
+    {
+        return view('user.form');
+    }
+
+    public function save(Request $request)
+    {
+        die(var_dump($request));
+        // Convalida i dati del modulo
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+    
+        // Crea un nuovo utente
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password')); // Assicurati di crittografare la password
+    
+        // Salva l'utente nel database
+        $user->save();
+        if(!$user->save()){
+            die(var_dump($user));
+        }
+
+        // Opzionalmente, puoi aggiungere un messaggio di successo
+        return redirect()->route('welcome')->with('success', 'Utente creato con successo!');
+    }
+    
 }
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ 
+Route::get('/user', [UserController::class, 'create']);
+
+Route::get('/user/save', [UserController::class, 'save']);
